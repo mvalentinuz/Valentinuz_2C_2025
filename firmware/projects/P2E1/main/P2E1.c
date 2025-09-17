b600ea337286fd6999e20d930a936ef5f62d7b9d
mvalentinuz <maurovalentinuz@gmail.com>
2025-09-17 11:21:53 -0300

    Finalización P2E1 - 17/9/2025

--- firmware/projects/P2E1/main/P2E1.c
+++ firmware/projects/P2E1/main/P2E1.c
@@ -1,24 +1,28 @@
 /*! @mainpage Template
  *
- * @section genDesc General Description
+ * @section genDesc Descripción General
  *
- * This section describes how the program works.
+ * Este programa mide la distancia con un sensor ultrasónico HC-SR04 y muestra el valor en un display LCD.
+ * Además, enciende LEDs según la distancia medida y permite controlar el funcionamiento con teclas.
  *
- * <a href="https://drive.google.com/...">Operation Example</a>
+ * <a href="https://drive.google.com/file/d/1yIPn12GYl-s8fiDQC3_fr2C4CjTvfixg/view">Ejemplo de operación</a>
  *
  * @section hardConn Hardware Connection
  *
  * |    Peripheral  |   ESP32   	|
  * |:--------------:|:--------------|
- * | 	PIN_X	 	| 	GPIO_X		|
+ * | GPIO_3			| ECHO			|
+ * | GPIO_2			| TRIGGER		|
+ * | +5V			| +5V			|
+ * | GND			| GND			|
  *
  *
  * @section changelog Changelog
  *
  * |   Date	    | Description                                    |
  * |:----------:|:-----------------------------------------------|
- * | 12/09/2023 | Document creation		                         |
- *
+ * | 10/09/2025 | Creación del documento                         |
+ * | 17/09/2025 | Finalización del desarrollo                    |
  * @author Mauro Valentinuz (maurovalentinuz@gmail.com)
  *
  */
@@ -36,53 +40,126 @@
 #include "lcditse0803.h"
 /*==================[macros and definitions]=================================*/
 #define CONFIG_PERIODO_MEDICION 1000
+#define CONFIG_PERIODO_TECLAS 100
 /*==================[internal data definition]===============================*/
-TaskHandle_t led1_task_handle = NULL;
-TaskHandle_t led2_task_handle = NULL;
-TaskHandle_t led3_task_handle = NULL;
+/// @brief Variable que almacena la distancia medida.
 uint16_t distancia = 0;
+/// @brief Variable que indica si el sistema está activado.
+bool activar = true;
+/// @brief Variable que indica si se debe sostener la lectura.
+bool sostener = false;
+/// @brief Variable que almacena la tecla presionada.
+uint8_t tecla;
 /*==================[internal functions declaration]=========================*/
+/// @brief Tarea que mide la distancia usando el sensor HC-SR04.
+/// @param pvParameter Parámetro de la tarea (no se usa).
 static void medirDistancia(void *pvParameter)
 {
-	while(true){
-		distancia= HcSr04ReadDistanceInCentimeters();
-		vTaskDelay(1000 / portTICK_PERIOD_MS);
-	}
-}
-
-static void prenderLedSegunDistancia(void *pvParameter)
-{
-	while(true){
-		if(distancia < 10){
-			LedOff(LED_1);
-			LedOff(LED_2);
-			LedOff(LED_3);
-		}else if(distancia >= 10 && distancia < 20){
-			LedOn(LED_1);
-			LedOff(LED_2);
-			LedOff(LED_3);
-		}else if(distancia >= 20 && distancia < 30){
-			LedOn(LED_1);
-			LedOn(LED_2);
-			LedOff(LED_3);
-		}else if(distancia >= 30){
-			LedOn(LED_1);
-			LedOn(LED_2);
-			LedOn(LED_3);
+	while (true)
+	{
+		if (activar)
+		{
+			distancia = HcSr04ReadDistanceInCentimeters();
 		}
 		vTaskDelay(CONFIG_PERIODO_MEDICION / portTICK_PERIOD_MS);
 	}
 }
 
+/// @brief Tarea que prende los LEDs según la distancia medida.
+/// @param pvParameter Parámetro de la tarea (no se usa).
+static void prenderLedSegunDistancia(void *pvParameter)
+{
+	while (true)
+	{
+		if (activar)
+		{
+			if (distancia < 10)
+			{
+				LedOff(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
+			}
+			else if (distancia >= 10 && distancia < 20)
+			{
+				LedOn(LED_1);
+				LedOff(LED_2);
+				LedOff(LED_3);
+			}
+			else if (distancia >= 20 && distancia < 30)
+			{
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOff(LED_3);
+			}
+			else if (distancia >= 30)
+			{
+				LedOn(LED_1);
+				LedOn(LED_2);
+				LedOn(LED_3);
+			}
+		}
+		else
+		{
+			LedOff(LED_1);
+			LedOff(LED_2);
+			LedOff(LED_3);
+		}
+		vTaskDelay(CONFIG_PERIODO_MEDICION / portTICK_PERIOD_MS);
+	}
+}
+
+/// @brief Tarea que lee el estado de las teclas y actualiza variables de control.
+/// @param pvParameter Parámetro de la tarea (no se usa).
+static void leerTeclas(void *pvParameter)
+{
+	while (true)
+	{
+		tecla = SwitchesRead();
+		switch (tecla)
+		{
+		case SWITCH_1:
+			activar = !activar;
+			break;
+		case SWITCH_2:
+			sostener = !sostener;
+			break;
+		}
+		vTaskDelay(CONFIG_PERIODO_TECLAS / portTICK_PERIOD_MS);
+	}
+}
+
+/// @brief Tarea que muestra la distancia en el display LCD.
+/// @param pvParameter Parámetro de la tarea (no se usa).
+static void mostrarDistanciaEnLcd(void *pvParameter)
+{
+	while (true)
+	{
+		if (activar)
+		{
+			if (!sostener)
+			{
+				LcdItsE0803Write(distancia);
+			}
+		}
+		else
+		{
+			LcdItsE0803Off();
+		}
+		vTaskDelay(CONFIG_PERIODO_MEDICION / portTICK_PERIOD_MS);
+	}
+}
 /*==================[external functions definition]==========================*/
+/// @brief Función main de la aplicación. Inicializa periféricos y crea tareas.
 void app_main(void)
 {
-	uint8_t teclas;
 	LedsInit();
 	SwitchesInit();
-	HcSr04Init();
+	HcSr04Init(GPIO_3, GPIO_2);
 	LcdItsE0803Init();
-	xTaskCreate(medirDistancia, "Medir Distancia", 2048, NULL, 2, NULL);
-	xTaskCreate(prenderLedSegunDistancia, "Prender LED Segun Distancia", 2048, NULL, 2, NULL);
+
+	xTaskCreate(&medirDistancia, "Medir Distancia", 512, NULL, 5, NULL);
+	xTaskCreate(&prenderLedSegunDistancia, "Prender LED Segun Distancia", 512, NULL, 5, NULL);
+	xTaskCreate(&leerTeclas, "Leer Teclas", 512, NULL, 5, NULL);
+	xTaskCreate(&mostrarDistanciaEnLcd, "Mostrar Distancia en LCD", 512, NULL, 5, NULL);
 }
 /*==================[end of file]============================================*/
